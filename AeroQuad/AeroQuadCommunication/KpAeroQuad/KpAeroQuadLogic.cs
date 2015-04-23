7d27457cc24c44d2a3e76b094bb1d8dd98a12689
2mik <m_shir@mail.ru>
2015-04-23 18:38:27 +0300

    KpAeroQuad

--- AeroQuad/AeroQuadCommunication/KpAeroQuad/KpAeroQuadLogic.cs
+++ AeroQuad/AeroQuadCommunication/KpAeroQuad/KpAeroQuadLogic.cs
@@ -12,6 +12,15 @@ namespace Scada.Comm.KP
             InitArrays(2, 0);
             KPParams[0] = new Param(1, "Связь");
             KPParams[1] = new Param(2, "Принятых сообщений");
+            KPParams[2] = new Param(3, "Roll Gyro Rate");
+            KPParams[3] = new Param(4, "Pitch Gyro Rate");
+            KPParams[4] = new Param(5, "Yaw Gyro Rate");
+            KPParams[5] = new Param(6, "Accel X Axis");
+            KPParams[6] = new Param(7, "Accel Y Axis");
+            KPParams[7] = new Param(8, "Accel Z Axis");
+            KPParams[8] = new Param(9, "Mag Raw Value X Axis");
+            KPParams[9] = new Param(10, "Mag Raw Value Y Axis");
+            KPParams[10] = new Param(11, "Mag Raw Value Z Axis");
         }
 
         public override void Session()
